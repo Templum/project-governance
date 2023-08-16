4bf2e2e0d4e5189d11190eff1f95e7345f533eb1
Templum <templum.dev@gmail.com>
2023-08-16 13:43:17 +0000

    :bookmark: Initial Version

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { getInput, setFailed, setOutput } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+async function main() {
+  try {
+    const accessToken = getInput('github-token');
+    const { owner, repo } = context.repo;
+    const issue = context.payload?.issue?.number ?? -1;
+
+    // Url is taken based on GITHUB_API_URL
+    const client = getOctokit(accessToken);
+
+    const { data } = await client.rest.issues.createComment({
+      issue_number: issue,
+      owner,
+      repo,
+      body: `An friendly hello from ${context.action} and thanks for raising a PR.`,
+    });
+
+    setOutput('comment-id', data.id);
+  } catch (error) {
+    const reason =
+      error instanceof Error
+        ? error
+        : typeof error?.toString() === 'function'
+        ? error.toString()
+        : 'Unknown issue occurred';
+
+    setFailed(reason);
+  }
+}
+
+main();
